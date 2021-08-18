1c7e5fc10ba9682c1225b6254f5b7aaf3d1296be
Neyliks <plgrigor082@gmail.com>
2021-08-18 13:24:31 +0300

    reset

--- src/ParticleBackground.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Particles from "react-particles-js"
-import bgsettings from "./bgsettings"
-
-export default function ParticleBackground() {
-    return (
-        <Particles params={bgsettings}></Particles>
-    );
-}
