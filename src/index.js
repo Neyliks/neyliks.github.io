04cf30a07b1b27ecba70d60c9195c204287e8cd4
Neyliks <plgrigor082@gmail.com>
2021-08-18 12:05:45 +0300

    basics 2

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as HashRouter, Route, Switch } from 'react-router-dom';
+import Header from "./components/header";
+import Footer from "./components/footer";
 
-ReactDOM.render(
+
+const routing = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <HashRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+      <Footer />
+  </HashRouter>
+  </React.StrictMode>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
