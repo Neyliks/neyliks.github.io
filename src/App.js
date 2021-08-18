173063bb547aa1adacc3ed0a842c587bdd83f5e8
Neyliks <plgrigor082@gmail.com>
2021-08-18 13:02:56 +0300

    stress test buggy version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import ParticleBackground from "./ParticleBackground";
 
 
 function App() {
   return (
-    <div className="App">
-      <p>Welcome to Neyliks Home!</p>
-      <p>I am just creating some maybe useless stuff and thats it :D</p>
+    <div>
+      <ParticleBackground />
+      <Home />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div id="center_all">
+      <div className="center_all">
+        <h1 className="custom-subTitle">Welcome to Neyliks Home!</h1>
+        <p className="custom-subTitle">I am just creating some maybe useless stuff and thats it :D</p>
+      </div>
     </div>
   );
 }
